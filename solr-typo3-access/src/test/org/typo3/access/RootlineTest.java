5db22d492db804df2bc4ad06b2026f94180b8899
Ingo Renner <ingo.renner@gmail.com>
2015-03-11 20:27:39 -0700

    Update copyright year
    Happy New Year!

--- solr-typo3-access/src/test/org/typo3/access/RootlineTest.java
+++ solr-typo3-access/src/test/org/typo3/access/RootlineTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2011 Ingo Renner <ingo@typo3.org>
+ * Copyright 2010-2015 Ingo Renner <ingo@typo3.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
